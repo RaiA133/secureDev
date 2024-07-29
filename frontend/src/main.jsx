1a5f3eebee601649f904bd17b494a5e48ca8107e
RaiA133 <rai010303@gmail.com>
2024-07-29 20:11:10 +0700

    first commit

---
+++ frontend/src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {NextUIProvider} from "@nextui-org/react";
+import App from './App.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <NextUIProvider>
+      <main className="dark text-foreground bg-background h-screen flex justify-center items-center">
+        <App />
+      </main>
+    </NextUIProvider>
+  </React.StrictMode>,
+)
