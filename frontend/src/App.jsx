16c0357e30de5d8dfbd726bfb20468f3790c8218
RaiA133 <rai010303@gmail.com>
2024-07-29 20:20:28 +0700

    improve: make component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,38 +1,11 @@
 import './App.css';
-import React, { useState } from 'react';
 import {
   Card, CardHeader, CardBody, CardFooter, Divider, Link, Image, Input,
   Accordion, AccordionItem, CheckboxGroup, Checkbox
 } from "@nextui-org/react";
+import FileStructure from './components/FileStructure';
 
 function App() {
-  const [filesStructure, setFilesStructure] = useState([
-    {
-      folderName: "Folder1",
-      files: ["File1", "File2", "File3"],
-      checked: false,
-      filesChecked: ["File1", "File2"]
-    },
-    {
-      folderName: "Folder2",
-      files: ["File1", "File2"],
-      checked: false,
-      filesChecked: ["File5", "File2"]
-    },
-    {
-      folderName: "Folder3",
-      files: ["File1", "File2", "File3", "File4", "File5"],
-      checked: false,
-      filesChecked: []
-    }
-  ]);
-
-  const handleFolderCheckboxChange = (folderIndex, checked) => {
-    const newFilesStructure = [...filesStructure];
-    newFilesStructure[folderIndex].checked = !filesStructure[folderIndex].checked;
-    newFilesStructure[folderIndex].filesChecked = filesStructure[folderIndex].checked ? [...newFilesStructure[folderIndex].files] : [];
-    setFilesStructure(newFilesStructure);
-  };
 
   return (
     <div className='flex flex-row gap-4'>
@@ -68,45 +41,7 @@ function App() {
         </CardFooter>
       </Card>
 
-      <Card className='w-[600px] p-5 py-10'>
-        <CardBody className='max-h-[600px] scroll-auto justify-center'>
-          <Accordion selectionMode="multiple" isCompact>
-            {filesStructure.map((folder, folderIndex) => (
-              <AccordionItem key={folderIndex} title={
-                <div className="flex items-center">
-                  <Checkbox
-                    isSelected={folder.checked}
-                    onChange={(checked) => {
-                      handleFolderCheckboxChange(folderIndex, checked)
-                    }}
-                  />
-                  <span>{folder.folderName}</span>
-                </div>
-              }>
-                <CheckboxGroup 
-                  value={folder.filesChecked} 
-                  onChange={(values) => {
-                    const newFilesStructure = [...filesStructure];
-                    newFilesStructure[folderIndex].filesChecked = values;
-                    newFilesStructure[folderIndex].checked = values.length === newFilesStructure[folderIndex].files.length;
-                    setFilesStructure(newFilesStructure);
-                  }} 
-                  className='mb-3 pl-5'
-                >
-                  {folder.files.map((file, fileIndex) => (
-                    <Checkbox
-                      key={fileIndex}
-                      value={file}
-                    >
-                      {file}
-                    </Checkbox>
-                  ))}
-                </CheckboxGroup>
-              </AccordionItem>
-            ))}
-          </Accordion>
-        </CardBody>
-      </Card>
+      <FileStructure />
       
     </div>
   );
---
+++ frontend/src/components/FileStructure.jsx
@@ -0,0 +1,83 @@
+import React, { useState } from 'react';
+import {
+	Card, CardBody,
+	Accordion, AccordionItem, CheckboxGroup, Checkbox
+} from "@nextui-org/react";
+
+function FileStructure() {
+	const [filesStructure, setFilesStructure] = useState([
+		{
+			folderName: "Folder1",
+			files: ["File1", "File2", "File3"],
+			checked: false,
+			filesChecked: ["File1", "File2"],
+		},
+		{
+			folderName: "Folder2",
+			files: ["File1", "File2"],
+			checked: true,
+			filesChecked: ["File1", "File2"],
+		},
+		{
+			folderName: "Folder3",
+			files: ["File1", "File2", "File3", "File4", "File5"],
+			checked: false,
+			filesChecked: [],
+		}
+	]);
+
+	const handleFolderCheckboxChange = (folderIndex, checked) => {
+		const newFilesStructure = [...filesStructure];
+		newFilesStructure[folderIndex].checked = !filesStructure[folderIndex].checked;
+		newFilesStructure[folderIndex].filesChecked = filesStructure[folderIndex].checked ? [...newFilesStructure[folderIndex].files] : [];
+		setFilesStructure(newFilesStructure);
+	};
+
+	return (
+		<div>
+
+			<Card className='min-w-[300px] max-w-[600px] p-5 py-10'>
+				<CardBody className='max-h-[600px] scroll-auto justify-center'>
+					<Accordion selectionMode="multiple" isCompact>
+						{filesStructure.map((folder, folderIndex) => (
+							<AccordionItem key={folderIndex} title={
+								<div className="flex items-center">
+									<Checkbox
+										isSelected={folder.checked}
+										onChange={(checked) => {
+											handleFolderCheckboxChange(folderIndex, checked)
+										}}
+									/>
+									<span>{folder.folderName}</span>
+								</div>
+							}>
+								<CheckboxGroup
+									value={folder.filesChecked}
+									onChange={(values) => {
+										const newFilesStructure = [...filesStructure];
+										newFilesStructure[folderIndex].filesChecked = values;
+										newFilesStructure[folderIndex].checked = values.length === newFilesStructure[folderIndex].files.length;
+										setFilesStructure(newFilesStructure);
+									}}
+									className='mb-3 pl-5'
+								>
+									{folder.files.map((file, fileIndex) => (
+										<Checkbox
+											key={fileIndex}
+											value={file}
+										>
+											{file}
+										</Checkbox>
+									))}
+								</CheckboxGroup>
+							</AccordionItem>
+						))}
+					</Accordion>
+				</CardBody>
+			</Card>
+
+		</div>
+	);
+}
+
+export default FileStructure;
