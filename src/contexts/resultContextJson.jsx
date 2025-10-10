c793d0a097074ca6eb0d96ec168b5aab2907397c
RaiA133 <rai010303@gmail.com>
2025-10-10 16:17:22 +0700

    fix: update model

--- src/contexts/resultContextJson.jsx
+++ src/contexts/resultContextJson.jsx
@@ -38,7 +38,7 @@ export const ResultContextProvider = ({ children }) => {
           ];
 
           const model = genAI.getGenerativeModel({
-            model: "gemini-1.5-flash",
+            model: "gemini-2.5-flash",
             generationConfig: { responseMimeType: "application/json" },
             safetySettings,
           });
