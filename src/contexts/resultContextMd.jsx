8343ca7b91481615179d867ad9a89fc1cd98404e
Raie Aswajjillah <108562968+RaiA133@users.noreply.github.com>
2025-10-10 14:05:33 +0700

    Update resultContextMd.jsx

--- src/contexts/resultContextMd.jsx
+++ src/contexts/resultContextMd.jsx
@@ -30,7 +30,7 @@ export const ResultContextProvider = ({ children }) => {
           ];
 
           const model = genAI.getGenerativeModel({
-            model: "gemini-1.5-flash",
+            model: "gemini-2.5-flash-image",
             safetySettings,
           });
 
