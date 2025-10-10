188ddcfc6c5b6d5277691ee5debbfdb2513a81d8
Raie Aswajjillah <108562968+RaiA133@users.noreply.github.com>
2025-10-10 14:05:48 +0700

    Update fileImportContext.jsx

--- src/contexts/fileImportContext.jsx
+++ src/contexts/fileImportContext.jsx
@@ -25,7 +25,7 @@ export const FileImportContextProvider = ({ children }) => {
           setIsLoadingImport(true)
 
           const model = genAI.getGenerativeModel({
-            model: "gemini-1.5-flash",
+            model: "gemini-2.5-flash-image",
             generationConfig: { responseMimeType: "application/json" }
           });
   
